2a4b1716b04231ba44ae9f44ad3048039445bc53
Pedro Duca <testandoduca@gmail.com>
2024-06-25 12:31:10 -0300

    Update react-native-svg version

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="nativewind/types" />
+
+declare module '*.svg' {
+  import React from 'react'
+  import { SvgProps } from 'react-native-svg'
+
+  const content: JSX.Element<SvgProps>
+
+  export default content
+}
